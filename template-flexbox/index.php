a844707d02316812d0eaaad6579bf7c31c423f92
Dyjayny Silva <130498214+DyjaynySilva@users.noreply.github.com>
2023-04-18 15:50:16 -0300

    Template FlexBox

---
+++ template-flexbox/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="header">
+  <h2>Topo</h2>
+</div>
+
+<div class="row">
+  <div class="column side" style="background-color:#f09292;">Livros</div>
+  <div class="column middle" style="background-color:#ebbaba;">Músicas</div>
+  <div class="column side" style="background-color:#f09292;">Filmes</div>
+</div>
+
+<div class="footer">
+  <p>@Dyjayny</p>
+</div>
+
+</body>
+</html>
