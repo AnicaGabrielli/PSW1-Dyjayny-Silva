9ad39524902a708d234a90b93896147f2a2fb6ac
Dyjayny Silva <130498214+DyjaynySilva@users.noreply.github.com>
2023-04-18 15:47:04 -0300

    Template Grid

---
+++ template-grid/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>flex Box</title>
+</head>
+<body>
+
+<div class="grid-container">
+  <div class="header">
+    <img src="png-transparent-edward-cullen-forks-the-twilight-saga-youtube-logo-twilight-saga-text-logo-monochrom.svg" alt="logo" width="100px" left="20px">
+  </div>
+  
+  <div class="left" style="background-color: rgb(67, 73, 126);">Músicas</div>
+  <div class="middle" style="background-color:rgb(186, 206, 245);">Livros</div>  
+  <div class="right" style="background-color: rgb(67, 73, 126);">Filmes</div>
+  
+  <div class="footer">
+    <p>@Dyjayny 2023 </p>
+  </div>
+</div>
+    
+</body>
+</html>
